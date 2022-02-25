33950a35a0c07b419c432da1d1bbb06e08515567
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-02-25 22:12:02 +0700

    hay

---
+++ main.py
@@ -0,0 +1,16 @@
+#mengekstrak isi dari tuple(sequence unpacking)
+
+mahasiswa = ("merry","mamuju",18)
+nama,alamat,umur = mahasiswa
+
+print("nama :",nama)
+print("alamat :",alamat)
+print("umur :",umur)
+
+#kemudian contoh lain dari tuple yaitu
+
+nilai_uas = (80,90,100)
+
+print(max(nilai_uas))
+print(min(nilai_uas))
+print(len(nilai_uas))
